4d334dac9a5a1737d794e27d04288d83444fddae
JIANMOP <feiyangpeng0508@gmail.com>
2026-06-05 17:05:47 +0800

    uv mirror

--- ubuntu/zsh/scripts/function/uv.zsh
+++ ubuntu/zsh/scripts/function/uv.zsh
@@ -1,6 +1,7 @@
 # >>> uv mirror managed block >>>
-export UV_INSTALLER_GITHUB_BASE_URL="https://uv.agentsmirror.com/github"
-export UV_PYTHON_DOWNLOADS_JSON_URL="https://uv.agentsmirror.com/metadata/python-downloads.json"
-export UV_PYPY_INSTALL_MIRROR="https://uv.agentsmirror.com/pypy"
-export UV_DEFAULT_INDEX="https://uv.agentsmirror.com/pypi/simple"
+#export UV_INSTALLER_GITHUB_BASE_URL="https://uv.agentsmirror.com/github"
+#export UV_PYTHON_DOWNLOADS_JSON_URL="https://uv.agentsmirror.com/metadata/python-downloads.json"
+#export UV_PYPY_INSTALL_MIRROR="https://uv.agentsmirror.com/pypy"
+#export UV_DEFAULT_INDEX="https://uv.agentsmirror.com/pypi/simple"
 # <<< uv mirror managed block <<<
+#export UV_DEFAULT_INDEX="https://pypi.tuna.tsinghua.edu.cn/simple"
